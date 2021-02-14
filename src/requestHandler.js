8083eed8578705520ad581d10941aa24e91b2ccb
Neves <marcos.neves@gmail.com>
2021-02-14 12:11:23 -0300

    move logRequest to after it is converted

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -1,9 +1,11 @@
 import { convertRequest } from "./convertRequest.js"
 import { FetchEventDeno } from "./FetchEventDeno.js"
 import { decorateRequest } from "./decorateRequest.js"
+import { logRequest } from "./logRequest.js"
 
 export async function requestHandler(originalRequest) {
   const request = await convertRequest(originalRequest)
+  logRequest(request)
   decorateRequest(request)
   dispatchEvent(new FetchEventDeno("fetch", { request, originalRequest }))
 }
--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ import { listenAndServe } from "https://deno.land/std/http/server.ts"
 import { freezeDateNow } from "./compatibility/freezeDateNow.js"
 import "./compatibility/index.js"
 import { forEach } from "./forEach.js"
-import { logRequest } from "./logRequest.js"
 import { requestHandler } from "./requestHandler.js"
 
 const port = +(Deno.env.get("PORT") || "8787")
 
-listenAndServe({ port }, forEach(logRequest, freezeDateNow, requestHandler))
+listenAndServe({ port }, forEach(freezeDateNow, requestHandler))
 const date = new Date().toJSON()
 console.log()
 console.log(date + " Listening on: http://0.0.0.0:" + port)
